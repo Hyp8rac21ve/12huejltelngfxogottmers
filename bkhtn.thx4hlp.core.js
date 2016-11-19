17ff83393118b79ad77f06b4f248afcec4554568
Hyp8rac21ve <jeeugenebbj@gmail.com>
2016-11-19 13:41:19 +0400

    Add files via upload

---
+++ bkhtn.thx4hlp.core.js
@@ -0,0 +1,43 @@
+(function () {
+    var footer = document.createElement('footer');
+    footer.classList.add("text-center");
+    footer.setAttribute("id", "contacts");
+    footer.innerHTML = `<div class="footer-above">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-col col-md-4">
+                            <h3>Адрес</h3>
+                            <p>Казань</p>
+                            <p> ул.Копылова,9</p>
+                        </div>
+                        <div class="footer-col col-md-4">
+                            <h3>Социальные сети</h3>
+                            <ul class="list-inline">
+                                <li>
+                                    <a href="#"><i class="fa fa-fw fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/salon_xelen/" ><i class="fa fa-fw fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://vk.com/public132978081" ><i class="fa fa-fw fa-vk"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                       <div class="footer-col col-md-4">
+                            <h3>Телефон</h3>
+                            <p>8 843 259 28 78</p>
+                            <p>8 962 559 28 78</p>
+                        </div>
+                        <div class="footer-col col-md-4">
+                        </div>
+                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2239.4034777674924!2d49.08148861593457!3d55.855665780581006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41594d7c4662ae85%3A0x91e420d36e3733eb!2z0YPQuy4g0JrQvtC_0YvQu9C-0LLQsCwgOSwg0JrQsNC30LDQvdGMLCDQoNC10YHQvy4g0KLQsNGC0LDRgNGB0YLQsNC9LCA0MjAwMzY!5e0!3m2!1sru!2sru!4v1479037289063" width="100%" height="300px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                   </div>
+                   <br>
+			</div>
+            </div> 
+    `;
+
+document.body.appendChild(footer);
+
+})();
